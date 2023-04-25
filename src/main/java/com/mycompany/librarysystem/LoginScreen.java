8f9743aec54110593d0722434bbabc3357192bbf
Felix Pettersson <130138911+Foxyzz1@users.noreply.github.com>
2023-04-25 11:00:23 +0200

    Login

--- src/main/java/com/mycompany/librarysystem/LoginScreen.java
+++ src/main/java/com/mycompany/librarysystem/LoginScreen.java
@@ -1,7 +1,12 @@
 package com.mycompany.librarysystem;
 
+import java.sql.*;
+import java.util.ServiceLoader;
+
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -10,12 +15,13 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class LoginScreen extends Scene {
-    //private Scene homeScene;
+    private static final String DB_URL = "jdbc:postgresql://hattie.db.elephantsql.com:5432/oaehwzla";
+    private static final String DB_USER = "oaehwzla";
+    private static final String DB_PASSWORD = "aj3XjlSmghfN4LGUZE12mOfUTDaKXiJY";
 
     public LoginScreen(Stage stage, Scene startScene) {
         super(new GridPane(), 300, 150);
         GridPane grid = (GridPane) this.getRoot();
-        //this.homeScene = homeScene;
         grid.setPadding(new Insets(10, 10, 10, 10));
         grid.setVgap(8);
         grid.setHgap(10);
@@ -43,11 +49,45 @@ public class LoginScreen extends Scene {
         GridPane.setConstraints(loginButton, 1, 2);
 
         loginButton.setOnAction(e -> {
-            HomeScreen homeScene = new HomeScreen(stage, startScene);
-            stage.setScene(homeScene);
+            try {
+                if (validateUser(usernameInput.getText(), passwordInput.getText())) {
+                    HomeScreen homeScene = new HomeScreen(stage, startScene);
+                    stage.setScene(homeScene);
+                } else {
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Invalid username or password!");
+                    alert.showAndWait();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         });
 
         grid.getChildren().addAll(usernameLabel, usernameInput, passwordLabel, passwordInput, loginButton);
         stage.setTitle("Login Screen");
     }
+
+    private boolean validateUser(String username, String password) throws SQLException {
+        String query = "SELECT * FROM public.anv WHERE username = ? AND password = ?";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new SQLException("PostgreSQL JDBC driver not found");
+        }
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            return resultSet.next();
+        }
+    }
+
 }
