efe575e23a5039dbfa4f24c28432119e351bb070
Nyaforsakringar2 <62518732+Nyaforsakringar2@users.noreply.github.com>
2023-05-17 14:30:02 +0200

    Lagt till lämna tillbaka lånad bok

--- src/main/java/com/mycompany/librarysystem/LoanScene.java
+++ src/main/java/com/mycompany/librarysystem/LoanScene.java
@@ -3,6 +3,7 @@ package com.mycompany.librarysystem;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
@@ -10,10 +11,15 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class LoanScene extends Scene {
     private Scene startScene;
     User user;
+    private Map<String, UUID> itemMap = new HashMap<>();
+
 
     public LoanScene(Stage stage, Scene startScene, User user) {
         super(new BorderPane(), 700, 300);
@@ -30,10 +36,12 @@ public class LoanScene extends Scene {
         grid.setVgap(8);
         grid.setHgap(10);
 
-        // Create four ListView components
+        // Create ListView components
         ListView<String> listOverdue = new ListView<>();
         ListView<String> listCurrentLoans = new ListView<>();
         ListView<String> listFinishedLoans = new ListView<>();
+
+        Button returnButton = new Button("Return Selected");
 
         // Create labels for each list
         Label labelOverdue = new Label("Overdue");
@@ -47,11 +55,25 @@ public class LoanScene extends Scene {
         grid.add(listCurrentLoans, 1, 1);
         grid.add(label3, 0, 2);
         grid.add(listFinishedLoans, 1, 2);
+        grid.add(returnButton, 2, 1);
+
 
         // Add grid to center of BorderPane
         borderPane.setCenter(grid);
 
         fetchUserLoans(user, listOverdue, listCurrentLoans, listFinishedLoans);
+
+        returnButton.setOnAction(e -> {
+            String selectedItem = listCurrentLoans.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                UUID itemId = itemMap.get(selectedItem);
+                returnBook(user, itemId);
+                listCurrentLoans.getItems().remove(selectedItem);
+                listFinishedLoans.getItems().add(selectedItem);
+            }
+        });
+
+
 
         // Show scene
         stage.setScene(this);
@@ -71,9 +93,13 @@ public class LoanScene extends Scene {
             LocalDate today = LocalDate.now();
 
             while (resultSet.next()) {
+                UUID itemId = (UUID) resultSet.getObject("itemid");
                 String title = resultSet.getString("title");
                 boolean finished = resultSet.getBoolean("finished");
                 LocalDate returnDate = resultSet.getDate("datedue").toLocalDate();
+
+                itemMap.put(title, itemId);
+
 
                 if (finished) {
                     listFinishedLoans.getItems().add(title);
@@ -87,4 +113,19 @@ public class LoanScene extends Scene {
             ex.printStackTrace();
         }
     }
+    private void returnBook(User user, UUID itemId) {
+        String query = "UPDATE public.loan SET finished = true WHERE userid = ? AND itemid = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setObject(1, user.getUserid());
+            preparedStatement.setObject(2, itemId);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
